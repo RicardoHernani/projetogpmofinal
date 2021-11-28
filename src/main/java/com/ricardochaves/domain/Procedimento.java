1bef0c159b1bcc2a9ce5db042651e989042eacc7
RicardoHernani <ricardohernani@yahoo.com.br>
2021-11-27 21:19:00 -0300

    Criacao das classes de dominio

---
+++ src/main/java/com/ricardochaves/domain/Procedimento.java
@@ -0,0 +1,143 @@
+package com.ricardochaves.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Procedimento implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String id;
+	
+	
+	private Integer matricula;
+	
+	
+	private Date data;
+	
+	private Integer tipo;
+	private Integer premio;
+	
+	
+	private String codigo;
+	
+	
+	private Referencia referencia;
+	
+	
+	private Usuario usuario;
+	
+	public Procedimento() {
+	}
+
+	public Procedimento(String id, Integer matricula, Date data, Integer tipo, Integer premio, String codigo, Referencia referencia, Usuario usuario) {
+		super();
+		this.id = id;
+		this.matricula = matricula;
+		this.data = data;
+		this.tipo = tipo;
+		this.premio = premio;
+		this.codigo = codigo;
+		this.referencia = referencia;
+		this.usuario = usuario;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Integer getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Integer matricula) {
+		this.matricula = matricula;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	
+
+	public Integer getPremio() {
+		return premio;
+	}
+
+	public void setPremio(Integer premio) {
+		this.premio = premio;
+	}
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public Referencia getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(Referencia referencia) {
+		this.referencia = referencia;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Procedimento other = (Procedimento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
---
+++ src/main/java/com/ricardochaves/resources/UsuarioResource.java
@@ -0,0 +1,17 @@
+package com.ricardochaves.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/usuarios")
+public class UsuarioResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST funcionando";
+	}
+	
+
+}
